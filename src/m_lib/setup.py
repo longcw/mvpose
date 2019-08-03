3a28a967e1cdf182abaf72cf0726f4d397c6f344
longc-blade <longch1024@gmail.com>
2019-08-03 15:20:45 +0800

    fix setup.py

--- src/m_lib/setup.py
+++ src/m_lib/setup.py
@@ -2,8 +2,19 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy as np
+
+
+np_include = np.get_include()
+
 ext_modules = [
-    Extension("pictorial", ["pictorial.pyx"], libraries=["m"], extra_compile_args=["-ffast-math", "-O3"])
+    Extension(
+        "pictorial",
+        ["pictorial.pyx"],
+        include_dirs=[np_include],
+        libraries=["m"],
+        extra_compile_args=["-ffast-math", "-O3"],
+    )
 ]
 
 setup(name="pictorial", cmdclass={"build_ext": build_ext}, ext_modules=ext_modules)
