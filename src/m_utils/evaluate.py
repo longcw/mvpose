b58b0f00d07aeb866cf16dde8bf0de526527c828
longc-ubuntu <longch1024@gmail.com>
2019-10-11 14:38:26 +0800

    init dump detections

--- src/m_utils/evaluate.py
+++ src/m_utils/evaluate.py
@@ -3,14 +3,14 @@ import sys
 import os.path as osp
 import pickle
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-if __name__ == '__main__':
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+if __name__ == "__main__":
     if project_root not in sys.path:
         sys.path.append(project_root)
 import coloredlogs, logging
 
 logger = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG', logger=logger)
+coloredlogs.install(level="DEBUG", logger=logger)
 
 from src.models.model_config import model_cfg
 import time
@@ -32,7 +32,9 @@ from src.m_utils.numeric import vectorize_distance
 from src.m_utils.mem_dataset import MemDataset
 
 
-def is_right(model_start_point, model_end_point, gt_strat_point, gt_end_point, alpha=0.5):
+def is_right(
+    model_start_point, model_end_point, gt_strat_point, gt_end_point, alpha=0.5
+):
     bone_lenth = np.linalg.norm(gt_end_point - gt_strat_point)
     start_difference = np.linalg.norm(gt_strat_point - model_start_point)
     end_difference = np.linalg.norm(gt_end_point - model_end_point)
@@ -41,10 +43,10 @@ def is_right(model_start_point, model_end_point, gt_strat_point, gt_end_point, a
 
 def numpify(info_dicts):
     for info_dict in info_dicts.values():
-        info_dict['image_data'] = info_dict['image_data'].squeeze().numpy()
+        info_dict["image_data"] = info_dict["image_data"].squeeze().numpy()
         for person in info_dict[0]:
-            person['heatmap_data'] = person['heatmap_data'].squeeze().numpy()
-            person['cropped_img'] = person['cropped_img'].squeeze().numpy()
+            person["heatmap_data"] = person["heatmap_data"].squeeze().numpy()
+            person["cropped_img"] = person["cropped_img"].squeeze().numpy()
     return info_dicts
 
 
@@ -58,7 +60,9 @@ def evaluate(model, actor3D, range_, loader, is_info_dicts=False, dump_dir=None)
             if is_info_dicts:
                 info_dicts = numpify(imgs)
                 model.dataset = MemDataset(
-                    info_dict=info_dicts, camera_parameter=camera_parameter, template_name='Unified'
+                    info_dict=info_dicts,
+                    camera_parameter=camera_parameter,
+                    template_name="Unified",
                 )
                 poses3d = model._estimate3d(0, show=False)
             else:
@@ -66,31 +70,49 @@ def evaluate(model, actor3D, range_, loader, is_info_dicts=False, dump_dir=None)
                 for img_batch in imgs:
                     this_imgs.append(img_batch.squeeze().numpy())
                 poses3d = model.predict(
-                    imgs=this_imgs, camera_parameter=camera_parameter, template_name='Unified', show=False
+                    imgs=this_imgs,
+                    camera_parameter=camera_parameter,
+                    template_name="Unified",
+                    show=False,
                 )
         except Exception as e:
             logger.critical(e)
             poses3d = False
 
         for pid in range(len(actor3D)):
-            if actor3D[pid][img_id][0].shape == (1, 0) or actor3D[pid][img_id][0].shape == (0, 0):
+            if actor3D[pid][img_id][0].shape == (1, 0) or actor3D[pid][img_id][
+                0
+            ].shape == (0, 0):
 
                 continue
 
             if not poses3d:
                 check_result[img_id, pid, :] = -1
-                logger.error(f'Cannot get any pose in img:{img_id}')
+                logger.error(f"Cannot get any pose in img:{img_id}")
                 continue
             model_poses = np.stack([coco2shelf3D(i) for i in deepcopy(poses3d)])
             gt_pose = actor3D[pid][img_id][0]
             dist = vectorize_distance(np.expand_dims(gt_pose, 0), model_poses)
             model_pose = model_poses[np.argmin(dist[0])]
 
-            bones = [[0, 1], [1, 2], [3, 4], [4, 5], [6, 7], [7, 8], [9, 10], [10, 11], [12, 13]]
+            bones = [
+                [0, 1],
+                [1, 2],
+                [3, 4],
+                [4, 5],
+                [6, 7],
+                [7, 8],
+                [9, 10],
+                [10, 11],
+                [12, 13],
+            ]
             for i, bone in enumerate(bones):
                 start_point, end_point = bone
                 if is_right(
-                    model_pose[start_point], model_pose[end_point], gt_pose[start_point], gt_pose[end_point]
+                    model_pose[start_point],
+                    model_pose[end_point],
+                    gt_pose[start_point],
+                    gt_pose[end_point],
                 ):
                     check_result[img_id, pid, i] = 1
                     accuracy_cnt += 1
@@ -107,57 +129,77 @@ def evaluate(model, actor3D, range_, loader, is_info_dicts=False, dump_dir=None)
                 error_cnt += 1
     bone_group = OrderedDict(
         [
-            ('Head', np.array([8])),
-            ('Torso', np.array([9])),
-            ('Upper arms', np.array([5, 6])),
-            ('Lower arms', np.array([4, 7])),
-            ('Upper legs', np.array([1, 2])),
-            ('Lower legs', np.array([0, 3])),
+            ("Head", np.array([8])),
+            ("Torso", np.array([9])),
+            ("Upper arms", np.array([5, 6])),
+            ("Lower arms", np.array([4, 7])),
+            ("Upper legs", np.array([1, 2])),
+            ("Lower legs", np.array([0, 3])),
         ]
     )
 
     total_avg = np.sum(check_result > 0) / np.sum(np.abs(check_result))
-    person_wise_avg = np.sum(check_result > 0, axis=(0, 2)) / np.sum(np.abs(check_result), axis=(0, 2))
+    person_wise_avg = np.sum(check_result > 0, axis=(0, 2)) / np.sum(
+        np.abs(check_result), axis=(0, 2)
+    )
 
     bone_wise_result = OrderedDict()
     bone_person_wise_result = OrderedDict()
     for k, v in bone_group.items():
-        bone_wise_result[k] = np.sum(check_result[:, :, v] > 0) / np.sum(np.abs(check_result[:, :, v]))
-        bone_person_wise_result[k] = np.sum(check_result[:, :, v] > 0, axis=(0, 2)) / np.sum(
-            np.abs(check_result[:, :, v]), axis=(0, 2)
+        bone_wise_result[k] = np.sum(check_result[:, :, v] > 0) / np.sum(
+            np.abs(check_result[:, :, v])
         )
+        bone_person_wise_result[k] = np.sum(
+            check_result[:, :, v] > 0, axis=(0, 2)
+        ) / np.sum(np.abs(check_result[:, :, v]), axis=(0, 2))
 
     tb = PrettyTable()
     tb.field_names = (
-        ['Bone Group'] + [f'Actor {i}' for i in range(bone_person_wise_result['Head'].shape[0])] + ['Average']
+        ["Bone Group"]
+        + [f"Actor {i}" for i in range(bone_person_wise_result["Head"].shape[0])]
+        + ["Average"]
     )
     list_tb = [tb.field_names]
     for k, v in bone_person_wise_result.items():
 
-        this_row = [k] + [np.char.mod('%.4f', i) for i in v] + [np.char.mod('%.4f', np.sum(v) / len(v))]
-        list_tb.append([float(i) if isinstance(i, type(np.array([]))) else i for i in this_row])
+        this_row = (
+            [k]
+            + [np.char.mod("%.4f", i) for i in v]
+            + [np.char.mod("%.4f", np.sum(v) / len(v))]
+        )
+        list_tb.append(
+            [float(i) if isinstance(i, type(np.array([]))) else i for i in this_row]
+        )
         tb.add_row(this_row)
     this_row = (
-        ['Total']
-        + [np.char.mod('%.4f', i) for i in person_wise_avg]
-        + [np.char.mod('%.4f', np.sum(person_wise_avg) / len(person_wise_avg))]
+        ["Total"]
+        + [np.char.mod("%.4f", i) for i in person_wise_avg]
+        + [np.char.mod("%.4f", np.sum(person_wise_avg) / len(person_wise_avg))]
     )
     tb.add_row(this_row)
-    list_tb.append([float(i) if isinstance(i, type(np.array([]))) else i for i in this_row])
+    list_tb.append(
+        [float(i) if isinstance(i, type(np.array([]))) else i for i in this_row]
+    )
     if dump_dir:
         np.save(
             osp.join(
                 dump_dir,
-                time.strftime(str(model_cfg.testing_on) + "_%Y_%m_%d_%H_%M", time.localtime(time.time())),
+                time.strftime(
+                    str(model_cfg.testing_on) + "_%Y_%m_%d_%H_%M",
+                    time.localtime(time.time()),
+                ),
             ),
             check_result,
         )
         with open(
             osp.join(
                 dump_dir,
-                time.strftime(str(model_cfg.testing_on) + "_%Y_%m_%d_%H_%M.csv", time.localtime(time.time())),
+                time.strftime(
+                    str(model_cfg.testing_on) + "_%Y_%m_%d_%H_%M.csv",
+                    time.localtime(time.time()),
+                ),
             ),
-            'w',
+            "w",
         ) as f:
             writer = csv.writer(f)
             writer.writerows(list_tb)
@@ -167,12 +209,15 @@ def evaluate(model, actor3D, range_, loader, is_info_dicts=False, dump_dir=None)
     return check_result, list_tb
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', nargs='+', dest='datasets', required=True, choices=['Shelf', 'Campus'])
-    parser.add_argument('-dumped', nargs='+', dest='dumped_dir', default=None)
+    parser.add_argument(
+        "-d", nargs="+", dest="datasets", required=True, choices=["Shelf", "Campus"]
+    )
+    parser.add_argument("-dumped", nargs="+", dest="dumped_dir", default=None)
+    parser.add_argument("-workers", type=int, default=4, dest="workers")
     args = parser.parse_args()
 
     test_model = MultiEstimator(cfg=model_cfg)
@@ -180,12 +225,12 @@ if __name__ == '__main__':
     for dataset_idx, dataset_name in enumerate(args.datasets):
         model_cfg.testing_on = dataset_name
 
-        if dataset_name == 'Shelf':
+        if dataset_name == "Shelf":
             dataset_path = model_cfg.shelf_path
             test_range = range(300, 600)
             gt_path = dataset_path
 
-        elif dataset_name == 'Campus':
+        elif dataset_name == "Campus":
             dataset_path = model_cfg.campus_path
             test_range = [i for i in range(350, 471)] + [i for i in range(650, 751)]
             gt_path = dataset_path
@@ -193,25 +238,35 @@ if __name__ == '__main__':
         else:
             dataset_path = model_cfg.panoptic_ultimatum_path
             test_range = range(4500, 4900)
-            gt_path = osp.join(dataset_path, '..')
+            gt_path = osp.join(dataset_path, "..")
 
-        with open(osp.join(dataset_path, 'camera_parameter.pickle'), 'rb') as f:
+        with open(osp.join(dataset_path, "camera_parameter.pickle"), "rb") as f:
             camera_parameter = pickle.load(f)
         if args.dumped_dir:
             test_dataset = PreprocessedDataset(args.dumped_dir[dataset_idx])
-            logger.info(f"Using pre-processed datasets {args.dumped_dir[dataset_idx]} for quicker evaluation")
+            logger.info(
+                f"Using pre-processed datasets {args.dumped_dir[dataset_idx]} for quicker evaluation"
+            )
             test_loader = DataLoader(
-                test_dataset, batch_size=1, pin_memory=False, num_workers=6, shuffle=False
+                test_dataset,
+                batch_size=1,
+                pin_memory=False,
+                num_workers=args.workers,
+                shuffle=False,
             )
         else:
             test_dataset = BaseDataset(dataset_path, test_range)
             test_loader = DataLoader(
-                test_dataset, batch_size=1, pin_memory=True, num_workers=12, shuffle=False
+                test_dataset,
+                batch_size=1,
+                pin_memory=False,
+                num_workers=args.workers,
+                shuffle=False,
             )
 
-        actorsGT = scio.loadmat(osp.join(gt_path, 'actorsGT.mat'))
-        test_actor3D = actorsGT['actor3D'][0]
-        if dataset_name == 'Panoptic':
+        actorsGT = scio.loadmat(osp.join(gt_path, "actorsGT.mat"))
+        test_actor3D = actorsGT["actor3D"][0]
+        if dataset_name == "Panoptic":
             test_actor3D /= 100  # mm->m
         evaluate(
             test_model,
@@ -219,5 +274,6 @@ if __name__ == '__main__':
             test_range,
             test_loader,
             is_info_dicts=bool(args.dumped_dir),
-            dump_dir=osp.join(project_root, 'result'),
+            dump_dir=osp.join(project_root, "result"),
         )
+
